8b49be053cb9091a3a443ad73bb33309b610403f
Mostafa Rastegar <m.rastegar991@gmail.com>
2022-05-23 22:54:37 +0430

    Update Card.stories.tsx

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta as StoryMeta } from '@storybook/react/types-6-0';
-import { Card, Avatar } from 'antd';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Story, Meta as StoryMeta } from "@storybook/react/types-6-0";
+import { Card, Avatar, Button } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
 const { Meta } = Card;
 export default {
-  title: 'Example/ant d Card',
+  title: "Example/ant d Card",
   component: Card,
 } as StoryMeta;
 
@@ -14,11 +18,23 @@ const Template: Story = (args) => (
   <Card
     {...args}
     style={{ width: 300 }}
-    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-    actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" />]}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <SettingOutlined key="setting" />,
+      <EditOutlined key="edit" />,
+      <EllipsisOutlined key="ellipsis" />,
+    ]}
   >
+    <Button type="primary">salam</Button>
     <Meta
-      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+      avatar={
+        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+      }
       title="Card title"
       description="This is the description"
     />
